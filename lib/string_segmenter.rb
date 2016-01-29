c04e954b0fc5d48050cd2b594a44f8ca4c1bbe0d
Jeremy Stoll <j8r8my@gmail.com>
2016-01-29 15:07:43 -0600

    again

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,12 +1,17 @@
-def segment_string(str)
-    
+ require_relative "dictionary.rb" 
+
+ def segment_string(str)
+ 
+
   str_arr = str.split(//)
   resting_arr = []
   words_arr = []
   skip = 0
   index_of_last_found_word = 0
-  str_arr.length.times do
+  
+ str_arr.length.times do
        working_str_arr = str_arr[index_of_last_found_word..-1]
+       
         working_str_arr.each do |x|
         resting_arr << x
         
@@ -16,21 +21,27 @@ def segment_string(str)
             resting_arr = []
            
           elsif 
-            valid_word?(resting_arr.join) && skip == 1
-            skip = 0
+            valid_word?(resting_arr.join) && skip > 0
+            skip -= 1
+            
           end
+
         end
        
+
         if 
-          resting_arr.empty? == true
+          resting_arr.empty?
           return words_arr
-          
+        
         elsif 
-          resting_arr.empty? == false
+          !resting_arr.empty?
           resting_arr = []
-          skip = 1
+          skip += 1         
           words_arr.pop
           index_of_last_found_word = words_arr.join.split(//).length 
+          
         end
-    end    
+
+  end 
+     
 end  
